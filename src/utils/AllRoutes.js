39db7736413600968141106ce8f8e699924e4dc5
vinithuday <vinithrajbmu@gmail.com>
2023-03-16 23:02:25 +0100

    language change in filters and footer language

--- src/utils/AllRoutes.js
+++ src/utils/AllRoutes.js
@@ -29,3 +29,16 @@ const AllRoutes = (props) => {
 };
 
 export default AllRoutes;
+
+
+
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Help from "../components/Help/Help";
+import Home from "../components/Home/Home";
+import Services from "../components/Services/Services";
+import Trips from "../components/Trips/Trips";
+import Filters from "./Filters/Filters";
+
+
+
