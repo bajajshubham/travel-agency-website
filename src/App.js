51a2425c1c0231a842cab286b54aad09b066170a
Shubham Bajaj <shubham2bajaj@gmail.com>
2023-03-16 18:41:38 +0100

    child navbar under trips

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Body from "./containers/Body";
-import Filters from "./utils/Filters/Filters";
 import Footer from "./components/Footer/Footer";
 import AllRoutes from "./utils/AllRoutes";
 
@@ -11,7 +10,6 @@ function App() {
       <Header />
       <Body>
         <AllRoutes />
-        <Filters />
       </Body>
       <Footer />
     </div>
