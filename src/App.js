5dec6695319cff0bed2a3f967fc9f786a10d99ba
Shubham Bajaj <shubham2bajaj@gmail.com>
2023-03-17 00:04:57 +0100

    updated app.js file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Body from "./containers/Body";
-<<<<<<< HEAD
-import Filters from "./utils/Filters/Filters";
 import Footer from "./components/Footer/Footer";
 import AllRoutes from "./utils/AllRoutes";
-import Home from "./components/Home/Home";
 
-
-
-
-
-=======
-import Footer from "./components/Footer/Footer";
-import AllRoutes from "./utils/AllRoutes";
->>>>>>> merge/filter-accessibility
 
 function App() {
 
@@ -23,15 +12,8 @@ function App() {
       <Header />
       <Body>
         <AllRoutes />
-<<<<<<< HEAD
-        <Filters />
       </Body>
       <Footer />
-   
-=======
-      </Body>
-      <Footer />
->>>>>>> merge/filter-accessibility
     </div>
   );
 }
