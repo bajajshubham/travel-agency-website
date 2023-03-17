aa86af024183e2f314a46293b6e589e4202cf1c8
Shubham Bajaj <shubham2bajaj@gmail.com>
2023-03-17 09:11:00 +0100

    added logo to the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,9 +5,10 @@ import {
 } from "@mui/icons-material";
 import "./Header.css";
 import { Link, NavLink } from "react-router-dom";
-import * as AiIcons from 'react-icons/ai';
+import * as AiIcons from "react-icons/ai";
 import Accessibility from "../Accessibility/Accessibility";
 import { Select, MenuItem } from "@mui/material";
+import logo from "../../assets/logo/TG_PNG.png";
 
 const Header = (props) => {
   const [sidebar, setSidebar] = useState(false);
@@ -23,58 +24,71 @@ const Header = (props) => {
   const getBrightness_val = (bright) => {
     setBrightness(bright);
     console.log(bright + "App");
-  }
+  };
 
   const getContrast_val = (contrast) => {
     setContrast(contrast);
     console.log(contrast + "App");
-  }
+  };
 
   const getSaturate_val = (saturate) => {
     setSaturate(saturate);
     // console.log(contrast + "App");
-  }
+  };
 
   const getMonochrome_val = (gray) => {
     setMonochrome(gray);
     // console.log(contrast + "App");
-  }
+  };
 
   const getDSwitchState = (Dval) => {
     setDSwitch(Dval);
     // console.log(Dval + "Dapp");
-  }
+  };
 
   const getPSwitchState = (Pval) => {
     setPSwitch(Pval);
     // console.log(Pval + "Papp");
-  }
+  };
   const getTSwitchState = (Tval) => {
     setTSwitch(Tval);
-  }
+  };
   return (
     <div className="header">
       <div className="header-top-part">
         <div className="web">
+          <img className="logo" src={logo} alt="brand logo" />
           <h1>
             Travel<span style={{ color: "#E45E35" }}>GO</span>at
           </h1>
         </div>
         <div className="activity">
           <NavLink className="accessibility-icon">
-            <AccessibilityNewIcon onClick={showSidebar}/>
-            </NavLink>
-            <nav className={sidebar ? 'menu access-active' : 'menu'}>
-             <div className='menu-items'>
-            <Link>
-            <AiIcons.AiOutlineClose onClick={showSidebar} color={'white'} />
-            </Link>
-            <Accessibility sendBrightness={getBrightness_val} sendContrast={getContrast_val} sendSaturate={getSaturate_val} sendMonochrome={getMonochrome_val}
-            sendDSwitch={getDSwitchState} sendPSwitch={getPSwitchState} sendTSwitch={getTSwitchState}
-            brightness_val={brightness_val} contrast_val={contrast_val} saturate_val={saturate_val} monochrome_val={monochrome_val}
-            DSwitch={DSwitch} PSwitch={PSwitch} TSwitch={TSwitch}/>
-        </div>
-      </nav>
+            <AccessibilityNewIcon onClick={showSidebar} />
+          </NavLink>
+          <nav className={sidebar ? "menu access-active" : "menu"}>
+            <div className="menu-items">
+              <Link>
+                <AiIcons.AiOutlineClose onClick={showSidebar} color={"white"} />
+              </Link>
+              <Accessibility
+                sendBrightness={getBrightness_val}
+                sendContrast={getContrast_val}
+                sendSaturate={getSaturate_val}
+                sendMonochrome={getMonochrome_val}
+                sendDSwitch={getDSwitchState}
+                sendPSwitch={getPSwitchState}
+                sendTSwitch={getTSwitchState}
+                brightness_val={brightness_val}
+                contrast_val={contrast_val}
+                saturate_val={saturate_val}
+                monochrome_val={monochrome_val}
+                DSwitch={DSwitch}
+                PSwitch={PSwitch}
+                TSwitch={TSwitch}
+              />
+            </div>
+          </nav>
           <span>
             <LanguageIcon />
           </span>
@@ -93,18 +107,29 @@ const Header = (props) => {
       </div>
       <div className="header-bottom-part">
         <NavLink className="nav-items" to="/">
-          {props.lang === "En" ? "Home" : props.lang === "De" ? "Startseite" : "घर"}
+          {props.lang === "En"
+            ? "Home"
+            : props.lang === "De"
+            ? "Startseite"
+            : "घर"}
         </NavLink>
         <NavLink className="nav-items" to="/trips">
-          {props.lang === "En" ? "Trips" : props.lang === "De" ? "Reisen" : "यात्राएं"}
+          {props.lang === "En"
+            ? "Trips"
+            : props.lang === "De"
+            ? "Reisen"
+            : "यात्राएं"}
         </NavLink>
         <NavLink className="nav-items" to="/services">
-          {props.lang === "En" ? "Services" : props.lang === "De" ? "Dienstleistungen" : "सेवाएं"}
+          {props.lang === "En"
+            ? "Services"
+            : props.lang === "De"
+            ? "Dienstleistungen"
+            : "सेवाएं"}
         </NavLink>
         <NavLink className="nav-items" to="/help">
           {props.lang === "En" ? "Help" : props.lang === "De" ? "Hilfe" : "मदद"}
         </NavLink>
-      
       </div>
     </div>
   );
